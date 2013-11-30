95656cee19afc25ae1da60862617bfc70b5867fb
Jere Menichelli <j.menichelli@globant.com>
2013-11-30 11:35:57 -0300

    Methods created, html page created to test
    Html page created to test the scripts. Functions created: addClass,
    removeClass, hasClass and toggleClass

--- js/classing.js
+++ js/classing.js
@@ -1,10 +1,54 @@
-// First fn: hasClass
-/*
-function(element, class){
-	split class attr content
-	create flag
-	iterate to find coincidence
-		change flag and break iteration
+// Clasing.js
+// fallback for browser that don't support classList
+// by Jeremias Menichelli - http://jeremenichelli.github.com
+
+// Given an element, converts classes into an array
+var classToArray = function(el){
+	return el.getAttribute('class').split(/\s/);
+}
+
+// Given an array, returns a string
+var arrayToClass = function(cs){
+	var nc = "";
+	for (var i = 0; i < cs.length; i++) {
+		nc += cs[i];
+		if (i != cs.length-1) nc += " ";
+	};
+	return nc
+}
+
+// Checks if an element has a class
+var hasClass = function(el, c){
+	var cs = classToArray(el);
+	var flag = false;
+	for (var i = 0; i < cs.length; i++) {
+		if(cs[i] == c) flag = true;
+	};
 	return flag
 }
-*/
+
+// Adds a class (if there's not already there)
+var addClass = function(el, c){
+	if(!hasClass(el, c)){
+		var nc = el.getAttribute('class')+" "+c;
+		el.setAttribute('class', nc);
+	}
+}
+
+// Removes a class (if it's there)
+var removeClass = function(el, c){
+	if(hasClass(el, c)){
+		var cs = classToArray(el);
+		for (var i = 0; i < cs.length; i++) {
+			if(cs[i] == c){
+				cs.splice(i,1);
+			}
+		};
+		el.setAttribute('class', arrayToClass(cs));
+	}
+}
+
+// Toggles a class in an element
+var toggleClass = function(el, c){
+	hasClass(el, c) ? removeClass(el, c) : addClass(el, c);
+}
