372c0779a676fa4b6a7d1d59343a87e3d5a0416e
Jere Menichelli <jmenichelli@gmail.com>
2014-03-19 00:59:09 -0300

    Changes in core for prototype approach and new perf test.

--- js/classing.js
+++ js/classing.js
@@ -1,28 +1,17 @@
 // Clasing.js
 // fallback for browser that don't support classList
 // by Jeremias Menichelli - https://github.com/jeremenichelli
+(function(){
+	'use strict';
+	// Object to prototype
+	var els = (typeof HTMLElement !== "undefined") ? HTMLElement : Element;
 
-if (document.documentElement.classList){
-	// Assign function to classLIst ones
-	var hasClass = function(el, c){
-		return el.classList.contains(c)
-	}
-	var addClass = function(el, c){
-		return el.classList.add(c)
-	}
-	var removeClass = function(el, c){
-		return el.classList.remove(c)
-	}
-	var toggleClass = function(el, c){
-		return el.classList.toggle(c)
-	}
-} else {
 	// Given an element, converts classes into an array
-	var classToArray = function(el){
+	var _classToArray = function(el){
 		return el.getAttribute('class').split(/\s/);
 	}
 	// Given an array, returns a string
-	var arrayToClass = function(cs){
+	var _arrayToClass = function(cs){
 		var nc = "";
 		for (var i = 0; i < cs.length; i++) {
 			nc += cs[i];
@@ -31,8 +20,8 @@ if (document.documentElement.classList){
 		return nc
 	}
 	// Checks if an element has a class
-	var hasClass = function(el, c){
-		var cs = classToArray(el);
+	var _hasClass = function(el, c){
+		var cs = _classToArray(el);
 		var flag = false;
 		for (var i = 0; i < cs.length; i++) {
 			if(cs[i] == c) flag = true;
@@ -40,26 +29,63 @@ if (document.documentElement.classList){
 		return flag
 	}
 	// Adds a class (if there's not already there)
-	var addClass = function(el, c){
-		if(!hasClass(el, c)){
+	var _addClass = function(el, c){
+		if(!_hasClass(el, c)){
 			var nc = el.getAttribute('class')+" "+c;
 			el.setAttribute('class', nc);
 		}
 	}
 	// Removes a class (if it's there)
-	var removeClass = function(el, c){
-		if(hasClass(el, c)){
-			var cs = classToArray(el);
+	var _removeClass = function(el, c){
+		if(_hasClass(el, c)){
+			var cs = _classToArray(el);
 			for (var i = 0; i < cs.length; i++) {
 				if(cs[i] == c){
 					cs.splice(i,1);
 				}
 			};
-			el.setAttribute('class', arrayToClass(cs));
+			el.setAttribute('class', _arrayToClass(cs));
 		}
 	}
 	// Toggles a class in an element
-	var toggleClass = function(el, c){
-		hasClass(el, c) ? removeClass(el, c) : addClass(el, c);
+	var _toggleClass = function(el, c){
+		_hasClass(el, c) ? _removeClass(el, c) : _addClass(el, c);
 	}
-}
+	
+	if(document.documentElement.classList){
+		els.prototype.hasClass = function(c){
+			return this.classList.contains(c);
+		}
+
+		els.prototype.addClass = function(c){
+			this.classList.add(c);
+		}
+
+		els.prototype.removeClass = function(c){
+			this.classList.remove(c);
+		}
+
+		els.prototype.toggleClass = function(c){
+			this.classList.toggle(c)
+		}
+	} else {
+		els.prototype.hasClass = function(c){
+			return _hasClass(this, c);
+		}
+
+		els.prototype.addClass = function(c){
+			_addClass(this, c);
+		}
+
+		els.prototype.removeClass = function(c){
+			_removeClass(this, c);
+		}
+
+		els.prototype.toggleClass = function(c){
+			_toggleClass(this, c);
+		}
+	}
+})();
+
+
+
